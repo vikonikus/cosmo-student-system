89ba26ab33c8edd0caedb72e586c9efc7c81c644
konstantinovvn <v.konstantinov@haulmont.com>
2025-08-06 16:50:28 +0400

    Добавлена база данных на студентов и факультеты

---
+++ src/main/java/ru/system/student/database/Database.java
@@ -0,0 +1,18 @@
+package ru.system.student.database;
+
+import ru.system.student.model.Faculty;
+import ru.system.student.model.Passport;
+import ru.system.student.model.Student;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Database {
+  public final static Map<Passport, Student> STUDENTS = new HashMap<>();
+  private final static List<Faculty> FACULTIES =
+          List.of(new Faculty("Математический", "+7495555555", "math@mail.ru"),
+                  new Faculty("Физический", "+7495555566", "fiz@mail.ru"),
+                  new Faculty("Экономический", "+7495555577", "fin@mail.ru"));
+
+}
